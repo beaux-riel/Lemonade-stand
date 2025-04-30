9fc01f8b6ce32f599fad60e15ed8bf8fe4240d82
Beaux W. <beaux.walton@gmail.com>
2025-04-30 06:41:11 -0700

    Update HomePage.tsx

--- lemonade-map/src/pages/HomePage.tsx
+++ lemonade-map/src/pages/HomePage.tsx
@@ -40,7 +40,7 @@ const HomePage: React.FC = () => {
                 </Link>
                 <Link
                   to="/about"
-                  className="bg-lemonade-blue-dark hover:bg-lemonade-blue text-white font-bold py-3 px-6 rounded-lg"
+                  className="bg-lemonade-blue-dark hover:bg-lemonade-blue text-white font-bold py-3 px-6 rounded-lg text-center transition-colors duration-200"
                 >
                   Learn More
                 </Link>
