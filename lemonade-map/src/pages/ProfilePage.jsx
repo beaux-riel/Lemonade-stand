4fe3a5cf8ea4bef261b668ec7a06dd5fc823a35c
beaux <34432201+beaux-riel@users.noreply.github.com>
2025-05-03 06:36:33 -0700

    React router correction

--- lemonade-map/src/pages/ProfilePage.jsx
+++ lemonade-map/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { 
   Card, 
   Form, 
