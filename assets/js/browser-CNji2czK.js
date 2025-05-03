925c1ac5141240aaae2ea7c0aa0f5cac50f0bc79
beaux-riel <beaux.walton@gmail.com>
2025-05-03 14:32:37 +0000

    Deploying to gh-pages from @ beaux-riel/Lemonade-Map@65f9d65095cb1dcbc839ac33bb9991744a9dff61 🚀

---
+++ assets/js/browser-CNji2czK.js
@@ -0,0 +1,2 @@
+import{g as e}from"./vendor-react-CGzs7eOH.js";var o,t;const r=e(t?o:(t=1,o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),n=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{n as b};
+//# sourceMappingURL=browser-CNji2czK.js.map
